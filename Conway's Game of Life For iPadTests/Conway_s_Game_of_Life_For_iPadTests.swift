f2e770fe88cce9a614f706cfbaa4e0457d0c827b
Jayesh Ramana <Jayesh@jayeshs-macbook-pro.local>
2018-11-12 16:57:58 -0500

    Initial Commit

---
+++ Conway's Game of Life For iPadTests/Conway_s_Game_of_Life_For_iPadTests.swift
@@ -0,0 +1,34 @@
+//
+//  Conway_s_Game_of_Life_For_iPadTests.swift
+//  Conway's Game of Life For iPadTests
+//
+//  Created by Jayesh Ramana on 11/12/18.
+//  Copyright © 2018 Jayesh Ramana. All rights reserved.
+//
+
+import XCTest
+@testable import Conway_s_Game_of_Life_For_iPad
+
+class Conway_s_Game_of_Life_For_iPadTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
